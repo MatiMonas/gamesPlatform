924d2db201b752564106291e2d1b9173c3d390ee
matimonas <matutin97@gmail.com>
2021-08-29 14:48:35 -0300

    create .envs

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const errorHandler = require('./utils/middlewares/errorHandler');
+const setHeaders = require('./utils/middlewares/setheaders');
 
 require('./db.js');
 
@@ -14,22 +16,12 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+server.use(setHeaders);
+server.use(errorHandler);
 
-server.use('/', routes);
+//seteo las rutas de mi app
+server.use('/api', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
 
 module.exports = server;
