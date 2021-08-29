4a9d8edaee9c663b5331bc3d693fe7f98e6da4ce
matimonas <matutin97@gmail.com>
2021-08-29 19:59:22 -0300

    agrega constants

---
+++ api/src/utils/constants/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { API_KEY } = process.env;
+
+const GAMES_URL = `https://api.rawg.io/api/games?key=${API_KEY}`;
+const GENRES_URL = `https://api.rawg.io/api/genres?key=${API_KEY}`;
+const BASE_ID = `https://api.rawg.io/api/games/`;
+const SEARCH_URL = `https://api.rawg.io/api/games?search=`;
+
+module.exports = {
+  GAMES_URL,
+  GENRES_URL,
+  BASE_ID,
+  SEARCH_URL,
+};
