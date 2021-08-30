91e8fd5c80ac000ea3a1fa24c80a759266bcc9fe
matimonas <matutin97@gmail.com>
2021-08-30 20:51:44 -0300

    AL FIN ARREGLE EL BUG

--- api/src/Controllers/getAllVideogames.js
+++ api/src/Controllers/getAllVideogames.js
@@ -1,19 +1,43 @@
+require("dotenv").config();
 const { Videogame, GameGenre, Platform } = require("../db");
+const { API_KEY } = process.env;
+const { SEARCH_URL } = require("../utils/constants");
 const { Op } = require("sequelize");
+const axios = require("axios");
 
 const getAllVideogames = async (req, res, next) => {
     const { name } = req.query;
 
     if (name) {
+        let dataBaseGames = [];
         try {
-            const dataBaseGames = await Videogame.findAll(
+            dataBaseGames = await Videogame.findAll(
                 {
                     where: { name: { [Op.like]: `%${name}%` } },
                     include: GameGenre,
                 },
                 { include: Platform }
             );
-            res.json(dataBaseGames);
+        } catch (err) {
+            next(err);
+        }
+        try {
+            // const getGames = await axios.get(`https://api.rawg.io/api/games?search=gta&key=`)
+
+            const getGames = await axios.get(
+                `${SEARCH_URL}${name}&key=${API_KEY}`
+            );
+            console.log(getGames.data);
+            const APIGames = await getGames.data.results?.map((element) => {
+                let game = {
+                    id: element.id,
+                    name: element.name,
+                    genres: element.genres.map((genre) => genre.name),
+                };
+                return game;
+            });
+            const allGAMES = [...dataBaseGames, ...APIGames];
+            res.json(allGAMES);
         } catch (err) {
             next(err);
         }
@@ -33,3 +57,6 @@ const getAllVideogames = async (req, res, next) => {
 };
 
 module.exports = getAllVideogames;
+
+//api --> link imagen
+//bd --> link
--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,11 +1,9 @@
 const { Router } = require("express");
 
 const getAllVideogames = require("../Controllers/getAllVideogames");
-// const getVideogamesByName = require("../Controllers/getVideogamesByName");
 
 const router = Router();
 
-// router.get("/", getVideogamesByName);
 router.get("/", getAllVideogames);
 
 module.exports = router;
