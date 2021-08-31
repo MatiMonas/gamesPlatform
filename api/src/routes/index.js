cdff2276434a058bcf2df2b4497528db29b9e0bd
matimonas <matutin97@gmail.com>
2021-08-31 13:53:10 -0300

    back 99.99%

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,13 +3,11 @@ const { Router } = require("express");
 const GameRoutes = require("./videogame");
 const GamesRoutes = require("./videogames");
 const GameGenreRoutes = require("./gamegenre");
-const PlatformRoutes = require("./platform");
 
 const router = Router();
 
 router.use("/videogame", GameRoutes);
 router.use("/videogames", GamesRoutes);
 router.use("/gamegenre", GameGenreRoutes);
-router.use("/platform", PlatformRoutes);
 
 module.exports = router;
