fef4805a3af796efbe7d7a4a6870091500c75ade
matimonas <matutin97@gmail.com>
2021-09-01 14:49:23 -0300

    adds actions types

--- client/src/redux/actions/actionTypes.js
+++ client/src/redux/actions/actionTypes.js
@@ -1,1 +1,6 @@
 export const GET_GAMES = "GET_GAMES";
+export const SEARCH_BY_NAME = "SEARCH_BY_NAME";
+export const SEARCH_BY_ID = "SEARCH_BY_ID";
+export const GET_PLATFORMS = "GET_PLATFORMS";
+export const GET_GENRES = "GET_GENRES";
+export const POST_VIDEOGAME = "POST_VIDEOGAME";
