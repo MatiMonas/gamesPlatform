39a2ff7b98b954ed5e695eebfbe16a35e9115c2e
matimonas <matutin97@gmail.com>
2021-09-01 14:46:15 -0300

    only shows db genres

--- api/src/Controllers/API/getGameGenres.js
+++ api/src/Controllers/API/getGameGenres.js
@@ -5,26 +5,13 @@ const axios = require("axios");
 const { GameGenre } = require("../../db");
 
 const getGameGenres = async (_req, res) => {
-    const URL = GENRES_URL;
-
     try {
-        const getGenres = await axios.get(URL);
-        const resultGenres = getGenres.data?.results;
-
-        //si existe un genero en la api lo voy a agregar a mi base de datos
-
-        resultGenres &&
-            resultGenres.map(async (genre) => {
-                await GameGenre.findOrCreate({
-                    where: { name: genre.name },
-                });
-            });
-
         //muestro todos los generos que tenga en mi base de datos
         const getDBGenres = await GameGenre.findAll();
-        return res.json(getDBGenres).status(200);
+        res.json(getDBGenres).status(200);
     } catch (err) {
-        console.error(err);
+        res.sendStatus(404);
+        next(err);
     }
 };
 
