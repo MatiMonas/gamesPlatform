9223642d157acac3b8ba7c4b54b373335a7f61a0
matimonas <matutin97@gmail.com>
2021-09-01 14:45:12 -0300

    precarga base de datos

--- api/index.js
+++ api/index.js
@@ -20,10 +20,28 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { PORT } = require("./src/utils/config");
+const { GENRES_URL } = require("./src/utils/constants");
+const axios = require("axios");
+const { GameGenre } = require("./src/db");
 
 // Syncing all the models at once.
 conn.sync({ force: true })
     .then(async () => {
+        const URL = GENRES_URL;
+
+        try {
+            const getGenres = await axios.get(URL);
+            const resultGenres = getGenres.data?.results;
+
+            resultGenres &&
+                resultGenres.map(async (genre) => {
+                    await GameGenre.findOrCreate({
+                        where: { name: genre.name },
+                    });
+                });
+        } catch (err) {
+            console.error(err);
+        }
         server.listen(PORT, () => {
             console.log(`Listening at ${PORT}`); // eslint-disable-line no-console
         });
