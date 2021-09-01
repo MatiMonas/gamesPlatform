1e1bba1cfaed71ac07c8321214622dc385f4997b
matimonas <matutin97@gmail.com>
2021-09-01 14:48:21 -0300

    changes landing route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Home from "./Components/Home";
 import { Route, Switch } from "react-router-dom";
-import LandingPage from "./Components/Landing/LandingPage";
+import Landing from "./Components/Landing/Landing";
 
 function App() {
     return (
         <>
             {/* <Route path="/" component={Nav} /> */}
             <Switch>
-                <Route path="/" exact component={LandingPage} />
+                <Route path="/" exact component={Landing} />
                 <Route patch="/home" exact component={Home}></Route>
             </Switch>
         </>
