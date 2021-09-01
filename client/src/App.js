14c415e3391fc1beba21e7e2fb1ed4436b63bbf3
matimonas <matutin97@gmail.com>
2021-09-01 16:52:20 -0300

    Preloads games ni app

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,16 @@ import "./App.css";
 import Home from "./Components/Home";
 import { Route, Switch } from "react-router-dom";
 import Landing from "./Components/Landing/Landing";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getGames } from "./redux/actions/index";
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getGames());
+    }, [dispatch]);
     return (
         <>
             {/* <Route path="/" component={Nav} /> */}
