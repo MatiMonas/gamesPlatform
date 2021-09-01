951958015ba1d3728ee1e23cf4112c1747653bb3
matimonas <matutin97@gmail.com>
2021-09-01 14:48:35 -0300

    adds constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,9 +1,11 @@
 const constants = {
     BASE_URL: "http://localhost:3001/api/",
-    GAME_URL: `${this.BASE_URL}videogame/`,
+    GAME_URL: `${this.BASE_URL}videogame`,
+    GAME_URL_NAME: `${this.BASE_URL}videogame?name=`,
     GAME_ID_URL: `${this.BASE_URL}videogame/:id`,
     GAMES_URL: `${this.BASE_URL}videogames/`,
     GENRES_URL: `${this.BASE_URL}gamegenre/`,
+    PLATFORMS_URL: `${this.BASE_URL}platforms`,
 };
 
 export default constants;
