2377119388bb702a0bc50a0fe7933231ae8a00f6
matimonas <matutin97@gmail.com>
2021-09-02 15:43:04 -0300

    elimino paginacion de gamesbyname (no era necesaria)

--- client/src/Containers/GamesByName/GamesByName.jsx
+++ client/src/Containers/GamesByName/GamesByName.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import GameCards from "../../Components/GameCards/GameCards";
 import NavBar from "../../Components/NavBar/NavBar";
-import Pagination from "../../Components/Pagination/Pagination";
 import { searchByName } from "../../redux/actions";
 import style from "./GamesByName.module.css";
 
@@ -12,35 +11,15 @@ function GamesByName() {
     let { name } = useParams();
     const videogameNames = useSelector((state) => state.searchByName);
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage] = useState(15);
-
-    let lastItemPerPage = currentPage * itemsPerPage;
-    let firstItemPerPage = lastItemPerPage - itemsPerPage;
-    let currentPageItems = videogameNames.slice(
-        firstItemPerPage,
-        lastItemPerPage
-    );
-
     useEffect(() => {
         dispatch(searchByName(name));
     }, [dispatch, name]);
 
-    function pagination(e, num) {
-        e.preventDefault();
-        setCurrentPage(num);
-    }
-
     return (
         <div>
             <NavBar />
-            <Pagination
-                itemsPerPage={itemsPerPage}
-                totalGames={videogameNames.length}
-                pagination={pagination}
-            />
             <div className={style.mainContainer}>
-                <GameCards games={currentPageItems} />
+                <GameCards games={videogameNames} />
             </div>
         </div>
     );
