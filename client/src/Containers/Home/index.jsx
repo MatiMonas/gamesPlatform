ed910958a7075011e99ae67ba054157b69ff87e3
matimonas <matutin97@gmail.com>
2021-09-02 04:15:44 -0300

    agrega paginacion a home

--- client/src/Containers/Home/index.jsx
+++ client/src/Containers/Home/index.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./index.module.css";
 import GameCards from "../../Components/GameCards/GameCards";
-// import { Link } from "react-router-dom";
+import SearchBar from "../../Components/NavBar/NavBar";
+import { useSelector } from "react-redux";
+import Pagination from "../../Components/Pagination/Pagination";
 
 function Home() {
+    const allVideogames = useSelector((state) => state.videogames);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(15);
+
+    function pagination(e, num) {
+        e.preventDefault();
+        setCurrentPage(num);
+    }
+
+    let lastItemPerPage = currentPage * itemsPerPage;
+    let firstItemPerPage = lastItemPerPage - itemsPerPage;
+    let currentPageItems = allVideogames.slice(
+        firstItemPerPage,
+        lastItemPerPage
+    );
+    console.log(itemsPerPage, allVideogames.length);
     return (
-        <div className={style.mainContainer}>
-            <GameCards className={style.cardContainer} />
-        </div>
+        <>
+            <Pagination
+                itemsPerPage={itemsPerPage}
+                totalGames={allVideogames.length}
+                pagination={pagination}
+            />
+            <div className={style.mainContainer}>
+                <GameCards games={currentPageItems} />
+            </div>
+        </>
     );
 }
 
