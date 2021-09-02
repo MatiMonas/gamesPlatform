7e1c0bfa6a40319d25945b0d53395e7960a812c7
matimonas <matutin97@gmail.com>
2021-09-02 04:14:03 -0300

    corrige constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 export const BASE_URL = "http://localhost:3001/api/";
 export const GAME_URL = `${BASE_URL}videogame`;
-export const GAME_URL_NAME = `${BASE_URL}videogame?name=`;
-export const GAME_ID_URL = `${BASE_URL}videogame/:id`;
+export const GAME_URL_NAME = `${BASE_URL}videogames?name=`;
+export const GAME_ID_URL = `${BASE_URL}videogame/`;
 export const GAMES_URL = `${BASE_URL}videogames/`;
 export const GENRES_URL = `${BASE_URL}gamegenre/`;
-export const PLATFORMS_URL = `${BASE_URL}platforms`;
+export const PLATFORMS_URL = `${BASE_URL}platforms/`;
