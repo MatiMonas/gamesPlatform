845a534a05b94dec7c070f7a1e27871a16fc042b
matimonas <matutin97@gmail.com>
2021-09-01 21:04:53 -0300

    gameCards file finished

--- client/src/Components/GameCards/GameCards.jsx
+++ client/src/Components/GameCards/GameCards.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import style from "./GameCards.module.css";
+import GameCard from "../GameCard/GameCard.jsx";
 
 function GameCards() {
     const videogames = useSelector((state) => state.videogames);
 
     return (
         <>
-            <div className={style.cardContainer}>
-                <div className={style.imageContainer}>
-                    <div className={style.backgroundImage}></div>
-                </div>
+            <div className={style.container}>
+                {videogames?.map((game) => (
+                    <GameCard
+                        key={game.id}
+                        img={game.background_image}
+                        name={game.name}
+                        genre={game.genres.map((el) => {
+                            return <li key={el.id}>{el.name}</li>;
+                        })}
+                    />
+                ))}
             </div>
         </>
     );
