a2aca37676ea407722507510c77c3676bf760605
matimonas <matutin97@gmail.com>
2021-09-02 22:11:21 -0300

    componente gamedetail

---
+++ client/src/Components/videogameDetail/VideoGameDetail.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { searchById } from "../../redux/actions";
+
+function VideogameDetail() {
+    const videoGameDetail = useSelector((state) => state.searchById);
+    let { id } = useParams();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(searchById(id));
+        return () => dispatch(searchById());
+    }, [dispatch, id]);
+
+    return (
+        <>
+            {videoGameDetail?.msg ? (
+                <h1>{videoGameDetail.msg}</h1>
+            ) : videoGameDetail ? (
+                <>
+                    <div>
+                        <div>
+                            <div>
+                                <h1>Title</h1> {videoGameDetail.name}
+                            </div>
+                            <div>
+                                {" "}
+                                <img
+                                    src={videoGameDetail.background_image}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <h4>Rating</h4>
+                                {videoGameDetail.rating}
+                            </div>
+                            <div>
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: videoGameDetail.description,
+                                    }}
+                                ></div>
+                            </div>
+                            <p>
+                                Genres:
+                                {videoGameDetail.genres
+                                    ?.map((el) => el?.name)
+                                    .join(", ")}
+                            </p>
+                            <p>
+                                {videoGameDetail.platforms
+                                    ?.map((el) => el)
+                                    .join(", ")}
+                            </p>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <h1>Cargando...</h1>
+            )}
+        </>
+    );
+}
+
+export default VideogameDetail;
--- client/src/Components/videogameDetail/index,.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function VideogameDetail() {
-    return <div></div>;
-}
-
-export default VideogameDetail;
