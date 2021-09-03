f2780161ddf6df1906dc260028b5cd309b581110
matimonas <matutin97@gmail.com>
2021-09-02 22:13:38 -0300

    agrega constante dfault img

--- client/src/constants.js
+++ client/src/constants.js
@@ -5,3 +5,5 @@ export const GAME_ID_URL = `${BASE_URL}videogame/`;
 export const GAMES_URL = `${BASE_URL}videogames/`;
 export const GENRES_URL = `${BASE_URL}gamegenre/`;
 export const PLATFORMS_URL = `${BASE_URL}platforms/`;
+export const DEFAULT_IMG =
+    "https://www.muycomputer.com/wp-content/uploads/2016/06/Nintendo-64.jpg";
