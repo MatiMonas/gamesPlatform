5e9a689bcd3133b0991b599f776913dd42e48713
matimonas <matutin97@gmail.com>
2021-09-02 22:13:14 -0300

    adds link in game Card component

--- client/src/Components/GameCard/GameCard.jsx
+++ client/src/Components/GameCard/GameCard.jsx
@@ -1,30 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import style from "./GameCard.module.css";
 
 function GameCard(props) {
     return (
         <>
             <div className={style.mainContainer}>
-                <div className={style.cardContainer}>
-                    <div className={style.titleContainer}>
-                        <h3>{props.name}</h3>
-                    </div>
+                <Link to={`/details/${props.id}`}>
+                    <div className={style.cardContainer}>
+                        <div className={style.titleContainer}>
+                            <h3>{props.name}</h3>
+                        </div>
 
-                    <div className={style.imageContainer}>
-                        <div className={style.backgroundImage}>
-                            <img src={props.img} alt={props.name} />
+                        <div className={style.imageContainer}>
+                            <div className={style.backgroundImage}>
+                                <img src={props.img} alt={props.name} />
+                            </div>
+                        </div>
+                        <div className={style.propsContainer}>
+                            <div className={style.genreContainer}>
+                                <ul className={style.genresList}>
+                                    <p className={style.gameGenres}>
+                                        {props.genre}
+                                    </p>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                    <div className={style.propsContainer}>
-                        <div className={style.genreContainer}>
-                            <ul className={style.genresList}>
-                                <p className={style.gameGenres}>
-                                    {props.genre}
-                                </p>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                </Link>
             </div>
         </>
     );
