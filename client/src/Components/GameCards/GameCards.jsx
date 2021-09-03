1e6129183c17da9f759311765429035dce0320c6
matimonas <matutin97@gmail.com>
2021-09-02 22:12:58 -0300

    defaul background image

--- client/src/Components/GameCards/GameCards.jsx
+++ client/src/Components/GameCards/GameCards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
 import style from "./GameCards.module.css";
 import GameCard from "../GameCard/GameCard.jsx";
+import { DEFAULT_IMG } from "../../constants";
 
 function GameCards({ games }) {
     return (
@@ -9,8 +9,13 @@ function GameCards({ games }) {
             <div className={style.container}>
                 {games?.map((game) => (
                     <GameCard
+                        id={game.id}
                         key={game.id}
-                        img={game.background_image}
+                        img={
+                            game.background_image
+                                ? game.background_image
+                                : DEFAULT_IMG
+                        }
                         name={game.name}
                         genre={game.genres?.map((el) => {
                             return <li key={el.id}>{el.name}</li>;
