f7f3518f3017067005e52c645b1cc8af098becd4
matimonas <matutin97@gmail.com>
2021-09-02 22:10:07 -0300

    manejo de renderizado

--- client/src/Containers/GamesByName/GamesByName.jsx
+++ client/src/Containers/GamesByName/GamesByName.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import GameCards from "../../Components/GameCards/GameCards";
 import NavBar from "../../Components/NavBar/NavBar";
-import { searchByName } from "../../redux/actions";
+import { clearPage, searchByName } from "../../redux/actions";
 import style from "./GamesByName.module.css";
 
 function GamesByName() {
@@ -13,15 +13,26 @@ function GamesByName() {
 
     useEffect(() => {
         dispatch(searchByName(name));
+        return () => dispatch(clearPage());
     }, [dispatch, name]);
 
     return (
-        <div>
-            <NavBar />
-            <div className={style.mainContainer}>
-                <GameCards games={videogameNames} />
+        <>
+            <div>
+                <NavBar />
             </div>
-        </div>
+            <div>
+                <div className={style.mainContainer}>
+                    {videogameNames?.length ? (
+                        <GameCards games={videogameNames} />
+                    ) : videogameNames === undefined ? (
+                        <h1>Cargando...</h1>
+                    ) : (
+                        <h1>Mario se perdio en la selva</h1>
+                    )}
+                </div>
+            </div>
+        </>
     );
 }
 
