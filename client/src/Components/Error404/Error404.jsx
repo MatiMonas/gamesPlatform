0dc9d15461aa290be37bdc2cf5c1d79792b5bdd3
matimonas <matutin97@gmail.com>
2021-09-04 01:29:47 -0300

    agrego link a home

--- client/src/Components/Error404/Error404.jsx
+++ client/src/Components/Error404/Error404.jsx
@@ -1,8 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export const Error404 = () => {
     return (
-        <div>
-            <h3>Error404</h3>
-            <p>Not found</p>
-        </div>
+        <>
+            <div>
+                <h3>Error404</h3>
+                <p>Not found</p>
+                <Link to="/home">
+                    <h1>Start Over</h1>
+                </Link>
+            </div>
+        </>
     );
 };
