1b6d4a79866bd533e6681b9df580de653d2b43bd
matimonas <matutin97@gmail.com>
2021-09-04 01:31:22 -0300

    elimino comentarios

--- api/src/Controllers/API/getAPIVideogames.js
+++ api/src/Controllers/API/getAPIVideogames.js
@@ -10,44 +10,6 @@ const getAPIVideogames = async (req, res, next) => {
     */
 
     try {
-        // for (let i = 0; i <= 5; i++) {
-        //     let getAPIGames = (await axios.get(URL)).data;
-        //     let games = getAPIGames.results.map((el) => {
-        //         let gameData = {
-        //             id: el.id,
-        //             name: el.name,
-        //             background_image: el.background_image,
-        //             genres: el.genres.map((genre) => genre.name),
-        //         };
-        //         return gameData;
-        //     });
-        //     APIGames = [...APIGames, ...games];
-        //     URL = getAPIGames.next;
-        // }
-        // return APIGames;
-        // //NECESITO CONSEGUIR EL DATA.NEXT DE TODO
-        // let arrayOfPromises = [];
-        // let json = [];
-        // let pages = [1, 2, 3, 4, 5].map((n) => {
-        //     let newPage = Promise.resolve(
-        //         axios.get(
-        //             ` https://api.rawg.io/api/games?key=178ece2035ae486d97b97d8234643709&page=4`
-        //         )
-        //     );
-        //     arrayOfPromises.push(newPage);
-        // });
-        // let promises = await Promise.all(arrayOfPromises);
-        // promises.forEach((el) => {
-        //     json = [...json, ...el.data];
-        // });
-        //  let result = json.slice(0, 1);
-        // console.log("SOY RESULTADOOOOOOOOOO", result);
-        // return result;
-        // //   /*-------
-        // //   Imagen
-        // //   Nombre
-        // //   Géneros------*/
-        // //podria intentar pushear al arrayOfPromises el promise.resolve de cada pagina
         let page1 = (await axios.get(`${URL}&page_size=40`)).data;
         let page2 = (await axios.get(page1.next)).data;
         let page3 = (await axios.get(page2.next)).data;
