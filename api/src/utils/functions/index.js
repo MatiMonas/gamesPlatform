cbad304054a97bc491e921b4cb24af04a37f968e
matimonas <matutin97@gmail.com>
2021-09-04 01:30:37 -0300

    hice una funcion que no use todavia xd

--- api/src/utils/functions/index.js
+++ api/src/utils/functions/index.js
@@ -1,8 +1,33 @@
-const axios = require("axios");
-
-const getData = async (URL) => {
-    let getData = await axios.get(URL);
-    return getData.data;
+const orderArray = function (arr, property, order = "asc") {
+    if (!arr.length) {
+        return arr;
+    }
+    let rta = arr;
+    switch (order) {
+        case "asc":
+            rta.sort(function (a, b) {
+                if (a[property] > b[property]) {
+                    return 1;
+                }
+                if (a[property] < b[property]) {
+                    return -1;
+                }
+                return 0;
+            });
+            break;
+        case "desc":
+            rta.sort(function (a, b) {
+                if (a[property] < b[property]) {
+                    return 1;
+                }
+                if (a[property] > b[property]) {
+                    return -1;
+                }
+                return 0;
+            });
+            break;
+    }
+    return rta;
 };
 
-module.exports = getData;
+module.exports = orderArray;
