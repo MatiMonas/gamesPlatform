90fe70ffb729d81981c699c6cc6831df91c18031
matimonas <matutin97@gmail.com>
2021-09-04 01:29:33 -0300

    cambio disposicion de card

--- client/src/Components/GameCard/GameCard.jsx
+++ client/src/Components/GameCard/GameCard.jsx
@@ -5,26 +5,24 @@ import style from "./GameCard.module.css";
 function GameCard(props) {
     return (
         <>
-            <div className={style.mainContainer}>
+            <div className={style.cardContainer}>
                 <Link to={`/details/${props.id}`}>
-                    <div className={style.cardContainer}>
-                        <div className={style.titleContainer}>
-                            <h3>{props.name}</h3>
-                        </div>
+                    <div className={style.titleContainer}>
+                        <h3>{props.name}</h3>
+                    </div>
 
-                        <div className={style.imageContainer}>
-                            <div className={style.backgroundImage}>
-                                <img src={props.img} alt={props.name} />
-                            </div>
+                    <div className={style.imageContainer}>
+                        <div className={style.backgroundImage}>
+                            <img src={props.img} alt={props.name} />
                         </div>
-                        <div className={style.propsContainer}>
-                            <div className={style.genreContainer}>
-                                <ul className={style.genresList}>
-                                    <p className={style.gameGenres}>
-                                        {props.genre}
-                                    </p>
-                                </ul>
-                            </div>
+                    </div>
+                    <div className={style.propsContainer}>
+                        <div className={style.genreContainer}>
+                            <ul className={style.genresList}>
+                                <p className={style.gameGenres}>
+                                    {props.genre}
+                                </p>
+                            </ul>
                         </div>
                     </div>
                 </Link>
