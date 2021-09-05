3119f495e6f3ee52bf88f1a438abb72c304454b0
matimonas <matutin97@gmail.com>
2021-09-05 20:32:39 -0300

    FUNCIONA TODO HASTA ACA

--- client/src/Utils/orders.js
+++ client/src/Utils/orders.js
@@ -1,38 +1,27 @@
-export function orderAlphFunc(games, order) {
+export function orderAlph(games, order) {
     let gameInOrder;
-
     if (order === "AZ") {
         const arrayOrdered = games?.sort((a, b) => {
-            if (a.name < b.name) {
-                return 1;
-            }
-            if (a.name > b.name) {
-                return -1;
-            }
+            if (a.name > b.name) return 1;
+            if (a.name < b.name) return -1;
             return 0;
         });
-
         gameInOrder = [...arrayOrdered];
         return gameInOrder;
     }
     if (order === "ZA") {
         const arrayOrdered = games?.sort((a, b) => {
-            if (a.name > b.name) {
-                return 1;
-            }
-            if (a.name < b.name) {
-                return -1;
-            }
+            if (a.name < b.name) return 1;
+            if (a.name > b.name) return -1;
             return 0;
         });
         gameInOrder = [...arrayOrdered];
         return gameInOrder;
     }
-
     return games;
 }
 
-export function orderRatingFunc(games, order) {
+export function orderRating(games, order) {
     let gameInOrder;
     if (order === "asc") {
         const arrayOrdered = games.sort((a, b) => {
