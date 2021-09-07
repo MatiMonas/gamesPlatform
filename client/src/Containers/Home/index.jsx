fa8f0607934466e181b9990c251d2bd4ad63a5eb
matimonas <matutin97@gmail.com>
2021-09-07 00:38:57 -0300

    cleans code

--- client/src/Containers/Home/index.jsx
+++ client/src/Containers/Home/index.jsx
@@ -1,24 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {
-    orderAz,
-    orderZa,
-    moreRating,
-    lessRating,
-    noOrder,
-} from "../../redux/actions";
+import { noOrder, order, orderRating } from "../../redux/actions";
 import GameCards from "../../Components/GameCards/GameCards";
 import Pagination from "../../Components/Pagination/Pagination";
 import NavBar from "../../Components/NavBar/NavBar";
 import SideBar from "../../Components/SideBar/SideBar";
 import style from "./index.module.css";
-// import OrderRating from "../../Components/Filters/OrderRating/OrderRating";
 
 function Home() {
     const dispatch = useDispatch();
     const filteredVideogames = useSelector((state) => state.filteredVideogames);
-    // const genres = useSelector((state) => state.genres);
     const [, setOrder] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage] = useState(15);
@@ -36,13 +28,14 @@ function Home() {
     }
     const handleOrder = (e) => {
         if (e.target.value === "All") dispatch(noOrder());
-        if (e.target.value === "AZ") dispatch(orderAz(e.target.value));
-        if (e.target.value === "ZA") dispatch(orderZa(e.target.value));
-        if (e.target.value === "asc") dispatch(moreRating(e.target.value));
-        if (e.target.value === "des") dispatch(lessRating(e.target.value));
+        if (e.target.value === "AZ" || e.target.value === "ZA")
+            dispatch(order(e.target.value));
+
+        if (e.target.value === "asc" || e.target.value === "des")
+            dispatch(orderRating(e.target.value));
 
         setCurrentPage(1);
-        // setOrder(e.target.value);
+        setOrder(e.target.value);
     };
 
     return (
