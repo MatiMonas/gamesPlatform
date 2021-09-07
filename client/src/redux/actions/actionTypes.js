1d281eb9181c61ff8bc5343357a5a2f7de41a5fd
matimonas <matutin97@gmail.com>
2021-09-07 00:38:46 -0300

    clear actionTypes

--- client/src/redux/actions/actionTypes.js
+++ client/src/redux/actions/actionTypes.js
@@ -4,11 +4,15 @@ export const SEARCH_BY_ID = "SEARCH_BY_ID";
 export const GET_PLATFORMS = "GET_PLATFORMS";
 export const GET_GENRES = "GET_GENRES";
 export const POST_VIDEOGAME = "POST_VIDEOGAME";
+
 export const CLEAR_GAME_DETAIL = "CLEAR_GAME_DETAIL";
-export const ORDER_ASC = "ORDER_ASC"; //nombre y rating
-export const ORDER_DESC = "ORDER_DESC";
-export const ORDER_MORE_RATING = "ORDER_MORE_RATING,";
-export const ORDER_LESS_RATING = "ORDER_LESS_RATING,";
+
+export const ORDER = "ORDER";
+export const ORDER_RATING = "ORDER_RATING";
+
+export const ORDER_NAMES = "ORDER_NAMES";
+export const ORDER_RATING_NAMES = "ORDER_RATING_NAMES";
+
 export const FILTER_ORIGIN = "FILTER_ORIGIN";
 export const FILTER_GENRES = "FILTER_GENRES";
 export const NO_ORDER = "NO_ORDER";
